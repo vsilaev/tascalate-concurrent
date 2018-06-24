dbd0807d7ffc8d48de2baa584950cb7c9ebfa791
vsilaev <vsilaev@gmail.com>
2018-06-24 12:21:47 +0300

    Widening argument types of Promises.all/any/anyStrict/atLeast/atLeastStrict

--- src/test/java/net/tascalate/concurrent/PromisesTests.java
+++ src/test/java/net/tascalate/concurrent/PromisesTests.java
@@ -21,18 +21,41 @@ public class PromisesTests {
     }
     
     @Test
-    public void testGenericArgs() {
-        Promise<List<Number>> p = ttt(BigDecimal.valueOf(10), Long.valueOf(11));
+    public void testGenericArgsWithList() {
+        Promise<List<Number>> p = combineWithList(BigDecimal.valueOf(10), Long.valueOf(11));
         List<Number> v = p.join();
         assertTrue("Fisrt is BigDecimal ", v.get(0) instanceof BigDecimal);
         assertTrue("Second is Long ", v.get(1) instanceof Long);
     }
     
-    <T, U extends T, V extends T> Promise<List<T>> ttt(U a, V b) {
+    @Test
+    public void testGenericArgsWithArray() {
+        Promise<List<Number>> p = combineWithArray1(BigDecimal.valueOf(10), Long.valueOf(11));
+        List<Number> v = p.join();
+        assertTrue("Fisrt is BigDecimal ", v.get(0) instanceof BigDecimal);
+        assertTrue("Second is Long ", v.get(1) instanceof Long);
+    }
+
+    
+    <T, U extends T, V extends T> Promise<List<T>> combineWithList(U a, V b) {
         List<Promise<T>> promises = new ArrayList<>();
         promises.add(Promises.success(a));
         promises.add(Promises.success(b));
         Promise<List<T>> all = Promises.all(promises);
         return all;
+    }
+    
+    <T, U extends T, V extends T> Promise<List<T>> combineWithArray1(U a, V b) {
+        @SuppressWarnings("unchecked")
+        Promise<T>[] promises = new Promise[2];
+        promises[0] = Promises.success(a);
+        promises[1] = Promises.success(b);
+        Promise<List<T>> all = Promises.all(promises);
+        return all;        
+    }
+    
+    <T, U extends T, V extends T> Promise<List<T>> combineWithArray2(U a, V b) {
+        Promise<List<T>> all = Promises.all(Promises.success(a), Promises.success(b));
+        return all;        
     }
 }
