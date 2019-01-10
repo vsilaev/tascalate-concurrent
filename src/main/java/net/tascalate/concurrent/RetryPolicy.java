d5db98c121ffd8e52a9cd01c37a1c39eb1d76de4
vsilaev <vsilaev@gmail.com>
2019-01-10 12:37:32 +0300

    Add method to create initial policy

--- src/main/java/net/tascalate/concurrent/RetryPolicy.java
+++ src/main/java/net/tascalate/concurrent/RetryPolicy.java
@@ -85,6 +85,10 @@ public class RetryPolicy<T> {
     private final DelayPolicy<? super T> backoff;
     private final DelayPolicy<? super T> timeout;
 
+    public static <T> RetryPolicy<T> create() {
+        return new RetryPolicy<>(-1);
+    }
+    
     @SafeVarargs
     public final RetryPolicy<T> retryOn(Class<? extends Throwable>... retryOnThrowables) {
         return retryOn(Arrays.asList(retryOnThrowables));
