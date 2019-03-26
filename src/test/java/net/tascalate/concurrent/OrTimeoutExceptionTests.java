f37379c8aa4cd4b1e8011129553ef847f59a7bdb
vsilaev <vsilaev@gmail.com>
2019-03-26 10:34:31 +0300

    Test for exceptional handling of Promise.orTimeout

---
+++ src/test/java/net/tascalate/concurrent/OrTimeoutExceptionTests.java
@@ -0,0 +1,48 @@
+package net.tascalate.concurrent;
+
+import static org.junit.Assert.assertTrue;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrTimeoutExceptionTests {
+    
+    private TaskExecutorService executor;
+
+    @Before
+    public void setUp() {
+        executor = TaskExecutors.newFixedThreadPool(4);
+    }
+    
+    @After
+    public void tearDown() {
+        executor.shutdown();
+    }
+    
+    @Test
+    public void testExceptionalOrTimeout() {
+        Promise<?> p = 
+        CompletableTask
+            .submit(this::doTask, executor)
+            .orTimeout(Duration.ofSeconds(5L))
+            .whenComplete((res, err) -> {
+                if (res != null)
+                    System.out.println("RESULT: " + res);
+                else
+                    System.out.println("ERROR: " + err.getMessage());
+             });
+        p.join();
+    }
+    
+    private String doTask () throws Exception {
+        TimeUnit.SECONDS.sleep(3);
+        if (null != System.out)
+            throw new Exception("my error");
+        return "executed ok";
+    }
+
+}
